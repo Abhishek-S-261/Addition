5087d498ec97f55251de55a63a9debb72045d695
Abhishek-S-261 <abhisheksshettar@gmail.com>
2025-04-08 12:10:48 +0530

    Craeted

--- jav.java
+++ jav.java
@@ -4,9 +4,11 @@ public class jav {
     public static void main(String[] args) {
         int a=10;
         int b=20;
-        int sum=a-b;
+        int sum=a+b;
+        int sum1=a-b;
 
         System.out.println("The Result is "+sum);
+        System.out.println("The Result is "+sum1);
     }
 
 }
