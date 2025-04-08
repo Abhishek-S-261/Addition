02e18e3bc76206f710dc094fe0b1c631f33dc9ab
Abhishek-S-261 <abhisheksshettar@gmail.com>
2025-04-08 12:08:36 +0530

    Craeted

--- jav.java
+++ jav.java
@@ -5,8 +5,10 @@ public class jav {
         int a=10;
         int b=20;
         int sum=a+b;
+        int sum1=a-b;
 
         System.out.println("The Result is "+sum);
+        System.out.println("The Result is "+sum1);
     }
 
 }
