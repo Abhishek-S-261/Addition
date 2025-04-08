298aed1b37e83b989c42a9e4b74497b6174a52ba
Abhishek-S-261 <abhisheksshettar@gmail.com>
2025-04-08 11:58:04 +0530

    Created

--- jav.java
+++ jav.java
@@ -4,7 +4,7 @@ public class jav {
     public static void main(String[] args) {
         int a=10;
         int b=20;
-        int sum=a+b;
+        int sum=a-b;
 
         System.out.println("The Result is "+sum);
     }
